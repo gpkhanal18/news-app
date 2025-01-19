90e68e821a356d3c474f3cdea1ce5a5fb43d8df1
Gopal Khanal <gpkhanal18@gmail.com>
2025-01-18 22:39:37 -0700

    updates

---
+++ frontend/newsroom-frontend/src/environments/environment.testing.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: true,
+  apiUrl:
+    'http://newsroom-api-env-test.eba-zskxyjjm.us-east-2.elasticbeanstalk.com/api', // Update this with the actual Testing backend URL
+};
